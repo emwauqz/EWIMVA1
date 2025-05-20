bd1a7bdd76f82671436429f5b00d6ee868cef3c7
emwauqz <emwauqz@gmail.com>
2025-05-21 04:21:43 +0600

    n

--- ewimva-store/vite.config.ts
+++ ewimva-store/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,8 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
 });
